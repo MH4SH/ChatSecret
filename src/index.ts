904b8d8ddab23cf2d022f8767985a57c4bbb56ce
Marcon Willian Oliveira Neves <oi@marconwillian.dev>
2021-01-30 19:07:41 -0300

    chore: FInish base of cypt messages

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ import server from './server';
 
 server();
 // senha-chave
+
+// https://www.youtube.com/watch?v=mDc5xueL7rs&list=PLmIA3VZysEqQCi5xOJJDb5QIYgHwPhf_D&index=43
+// Cursos sobre IndexedDB
