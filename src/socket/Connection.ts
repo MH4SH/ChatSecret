8aa451e4f0da8c759ef5efa5446be4952e0b3b16
Marcon Willian Oliveira Neves <oi@marconwillian.dev>
2021-01-31 16:30:56 -0300

    chore: Remove auth

--- src/socket/Connection.ts
+++ src/socket/Connection.ts
@@ -4,8 +4,6 @@ import * as openpgp from 'openpgp';
 import fs from 'fs';
 import Redis from '../lib/Redis';
 
-import { verifySocker, SocketAuth } from '../middlewares/auth';
-
 export interface SocketQuery {
   userName: string;
 }
@@ -21,48 +19,107 @@ const connection = (server: Server): void => {
 
   console.log('Started monit listening!');
 
-  io.attach(server)
-    // .use(verifySocker)
-    .on('connection', (socket: SocketAuth) => {
-      const socketQuery = socket.handshake.query as SocketQuery;
+  io.attach(server).on('connection', (socket: socketIo.Socket) => {
+    const socketQuery = socket.handshake.query as SocketQuery;
 
-      console.log(`Client entrou connection [id=${socket.id}]`);
-      socket.join(socketQuery.userName);
+    console.log(`Client entrou connection [id=${socket.id}]`);
+    socket.join(socketQuery.userName);
 
-      Redis.hmset(`Users:${socketQuery.userName}:data`, {
-        lastAccess: new Date().getTime()
-      });
+    Redis.hmset(`Users:${socketQuery.userName}:data`, {
+      lastAccess: new Date().getTime()
+    });
 
-      Redis.keys(
-        `Contact:add:${socketQuery.userName}:per:*`,
-        (err, keys: string[]) => {
-          keys.forEach(key => {
-            Redis.hmget(key, 'contact', (errGet, data) => {
-              socket.emit('contact:new', data[0]);
-            });
+    Redis.keys(
+      `Contact:add:${socketQuery.userName}:per:*`,
+      (err, keys: string[]) => {
+        keys.forEach(key => {
+          Redis.hmget(key, 'contact', (errGet, data) => {
+            socket.emit('contact:new', data[0]);
           });
+        });
+      }
+    );
+
+    socket.on(
+      'contact:add',
+      async (contactEncrypted, timestamp = 1000): Promise<void | boolean> => {
+        const publicKeyUser = (
+          await new Promise<string[]>(resolve => {
+            Redis.hmget(
+              `Users:${socketQuery.userName}:data`,
+              'publicKey',
+              (err, data) => resolve(data)
+            );
+          })
+        )[0];
+
+        const decrypted = await openpgp.decrypt({
+          message: await openpgp.message.readArmored(contactEncrypted),
+          publicKeys: (await openpgp.key.readArmored(publicKeyUser)).keys,
+          privateKeys: [globalAny.__PRIVATE_KEY_SERVER__]
+        });
+
+        if (!decrypted.signatures[0].valid) {
+          return false;
         }
-      );
 
-      /**
-       * Verify is Online
-       */
+        const contact = JSON.parse(decrypted.data);
 
-      socket.on(
-        'contact:add',
-        async (contactEncrypted, timestamp = 1000): Promise<void | boolean> => {
-          const publicKeyUser = (
-            await new Promise<string[]>(resolve => {
-              Redis.hmget(
-                `Users:${socketQuery.userName}:data`,
-                'publicKey',
-                (err, data) => resolve(data)
-              );
-            })
-          )[0];
+        const [publicKeyDestination] = await new Promise<string[]>(resolve => {
+          Redis.hmget(`Users:${contact.user}:data`, 'publicKey', (err, data) =>
+            resolve(data)
+          );
+        });
+
+        if (publicKeyDestination === null) {
+          const { data: errorContact } = await openpgp.encrypt({
+            message: openpgp.message.fromText(
+              JSON.stringify({
+                status: 'error',
+                messageCode: '3',
+                message: 'Contato não existe!',
+                contact: contact.user
+              })
+            ),
+            publicKeys: (await openpgp.key.readArmored(publicKeyUser)).keys,
+            privateKeys: [globalAny.__PRIVATE_KEY_SERVER__]
+          });
+
+          socket.emit('contact:error', errorContact);
+        }
+
+        const { data: newContact } = await openpgp.encrypt({
+          message: openpgp.message.fromText(JSON.stringify(contact)),
+          publicKeys: (await openpgp.key.readArmored(publicKeyDestination))
+            .keys,
+          privateKeys: [globalAny.__PRIVATE_KEY_SERVER__]
+        });
+
+        Redis.hmset(`Contact:add:${contact.user}:per:${socketQuery.userName}`, {
+          contact: newContact,
+          dateStart: new Date().getTime()
+        });
+
+        socket.to(contact.user).emit('contact:new', newContact);
+
+        return true;
+      }
+    );
+
+    socket.on(
+      'contact:add:received',
+      async (contactReceived, timestamp = 1000): Promise<void | boolean> => {
+        try {
+          const publicKeyUser = await new Promise(resolve => {
+            Redis.hmget(
+              `Users:${socketQuery.userName}:data`,
+              'publicKey',
+              (err, data) => resolve(data[0])
+            );
+          });
 
           const decrypted = await openpgp.decrypt({
-            message: await openpgp.message.readArmored(contactEncrypted),
+            message: await openpgp.message.readArmored(contactReceived),
             publicKeys: (await openpgp.key.readArmored(publicKeyUser)).keys,
             privateKeys: [globalAny.__PRIVATE_KEY_SERVER__]
           });
@@ -71,91 +128,20 @@ const connection = (server: Server): void => {
             return false;
           }
 
-          const contact = JSON.parse(decrypted.data);
-
-          const [publicKeyDestination] = await new Promise<string[]>(
-            resolve => {
-              Redis.hmget(
-                `Users:${contact.user}:data`,
-                'publicKey',
-                (err, data) => resolve(data)
-              );
-            }
+          const contact: { to: string; from: string } = JSON.parse(
+            decrypted.data
           );
 
-          if (publicKeyDestination === null) {
-            const { data: errorContact } = await openpgp.encrypt({
-              message: openpgp.message.fromText(
-                JSON.stringify({
-                  status: 'error',
-                  messageCode: '3',
-                  message: 'Contato não existe!',
-                  contact: contact.user
-                })
-              ),
-              publicKeys: (await openpgp.key.readArmored(publicKeyUser)).keys,
-              privateKeys: [globalAny.__PRIVATE_KEY_SERVER__]
-            });
-
-            socket.emit('contact:error', errorContact);
-          }
-
-          const { data: newContact } = await openpgp.encrypt({
-            message: openpgp.message.fromText(JSON.stringify(contact)),
-            publicKeys: (await openpgp.key.readArmored(publicKeyDestination))
-              .keys,
-            privateKeys: [globalAny.__PRIVATE_KEY_SERVER__]
-          });
-
-          Redis.hmset(
-            `Contact:add:${contact.user}:per:${socketQuery.userName}`,
-            {
-              contact: newContact,
-              dateStart: new Date().getTime()
-            }
-          );
-
-          socket.to(contact.user).emit('contact:new', newContact);
+          Redis.del(`Contact:add:${contact.to}:per:${contact.from}`);
 
           return true;
+        } catch (err) {
+          console.log(err);
+          return false;
         }
-      );
-
-      socket.on(
-        'contact:add:received',
-        async (contactReceived, timestamp = 1000): Promise<void | boolean> => {
-          try {
-            const publicKeyUser = await new Promise(resolve => {
-              Redis.hmget(
-                `Users:${socketQuery.userName}:data`,
-                'publicKey',
-                (err, data) => resolve(data[0])
-              );
-            });
-
-            const decrypted = await openpgp.decrypt({
-              message: await openpgp.message.readArmored(contactReceived),
-              publicKeys: (await openpgp.key.readArmored(publicKeyUser)).keys,
-              privateKeys: [globalAny.__PRIVATE_KEY_SERVER__]
-            });
-
-            if (!decrypted.signatures[0].valid) {
-              return false;
-            }
-
-            const contact: { to: string; from: string } = JSON.parse(
-              decrypted.data
-            );
-
-            Redis.del(`Contact:add:${contact.to}:per:${contact.from}`);
-
-            return true;
-          } catch (err) {
-            console.log(err);
-          }
-        }
-      );
-    });
+      }
+    );
+  });
 };
 
 export default connection;
--- src/socket/Conversation.ts
+++ src/socket/Conversation.ts
@@ -2,10 +2,7 @@ import { v4 as uuid } from 'uuid';
 import { Server } from 'http';
 import * as socketIo from 'socket.io';
 import * as openpgp from 'openpgp';
-import fs from 'fs';
 import Redis from '../lib/Redis';
-
-import { verifySocker, SocketAuth } from '../middlewares/auth';
 
 export interface SocketQuery {
   userName: string;
@@ -22,104 +19,102 @@ const conversation = (server: Server): void => {
 
   console.log('Started monit listening!');
 
-  io.attach(server)
-    .use(verifySocker)
-    .on('connection', (socket: SocketAuth) => {
-      const socketQuery = socket.handshake.query as SocketQuery;
+  io.attach(server).on('connection', (socket: socketIo.Socket) => {
+    const socketQuery = socket.handshake.query as SocketQuery;
 
-      console.log(`Client entrou conversation [id=${socket.id}]`);
-      socket.join(socketQuery.userName);
+    console.log(`Client entrou conversation [id=${socket.id}]`);
+    socket.join(socketQuery.userName);
 
-      Redis.hmset(`Users:${socketQuery.userName}:data`, {
-        lastAccess: new Date().getTime()
-      });
-
-      Redis.keys(
-        `Message:to:${socketQuery.userName}:from:*`,
-        (err, keys: string[]) => {
-          keys.forEach(key => {
-            Redis.hmget(key, 'id', 'message', (errGet, data) => {
-              socket.emit('message:receive', { id: data[0], message: data[1] });
-            });
-          });
-        }
-      );
-
-      socket.on('disconnect', () => {
-        console.log(`Client saiu [id=${socket.id}]`);
-      });
-
-      socket.on(
-        'message:new',
-        async ({ to, message }, timestamp = 1000): Promise<void | boolean> => {
-          console.time('New Message');
-          const id = uuid();
-
-          const publicKeyTo = (
-            await new Promise<string[]>(resolve => {
-              Redis.hmget(`Users:${to}:data`, 'publicKey', (err, data) =>
-                resolve(data)
-              );
-            })
-          )[0];
-
-          const { data: messageToSend } = await openpgp.encrypt({
-            message: openpgp.message.fromText(
-              JSON.stringify({
-                itsMine: false,
-                contact: socketQuery.userName,
-                message
-              })
-            ),
-            publicKeys: (await openpgp.key.readArmored(publicKeyTo)).keys,
-            privateKeys: [globalAny.__PRIVATE_KEY_SERVER__]
-          });
-
-          Redis.hmset(`Message:to:${to}:from:${socketQuery.userName}:${id}`, {
-            id,
-            message: messageToSend,
-            dateSended: new Date().getTime()
-          });
-
-          socket.to(to).emit('message:receive', { id, message: messageToSend });
-          console.timeEnd('New Message');
-          return true;
-        }
-      );
-
-      socket.on(
-        'message:new:received',
-        async (messageReceived, timestamp = 1000): Promise<void | boolean> => {
-          const publicKeyUser = await new Promise(resolve => {
-            Redis.hmget(
-              `Users:${socketQuery.userName}:data`,
-              'publicKey',
-              (err, data) => resolve(data[0])
-            );
-          });
-
-          const decrypted = await openpgp.decrypt({
-            message: await openpgp.message.readArmored(messageReceived),
-            publicKeys: (await openpgp.key.readArmored(publicKeyUser)).keys,
-            privateKeys: [globalAny.__PRIVATE_KEY_SERVER__]
-          });
-
-          if (!decrypted.signatures[0].valid) {
-            return false;
-          }
-
-          const message: { id: string; to: string; from: string } = JSON.parse(
-            decrypted.data
-          );
-
-          Redis.del(
-            `Message:to:${message.to}:from:${message.from}:${message.id}`
-          );
-
-          return true;
-        }
-      );
+    Redis.hmset(`Users:${socketQuery.userName}:data`, {
+      lastAccess: new Date().getTime()
     });
+
+    Redis.keys(
+      `Message:to:${socketQuery.userName}:from:*`,
+      (err, keys: string[]) => {
+        keys.forEach(key => {
+          Redis.hmget(key, 'id', 'message', (errGet, data) => {
+            socket.emit('message:receive', { id: data[0], message: data[1] });
+          });
+        });
+      }
+    );
+
+    socket.on('disconnect', () => {
+      console.log(`Client saiu [id=${socket.id}]`);
+    });
+
+    socket.on(
+      'message:new',
+      async ({ to, message }, timestamp = 1000): Promise<void | boolean> => {
+        console.time('New Message');
+        const id = uuid();
+
+        const publicKeyTo = (
+          await new Promise<string[]>(resolve => {
+            Redis.hmget(`Users:${to}:data`, 'publicKey', (err, data) =>
+              resolve(data)
+            );
+          })
+        )[0];
+
+        const { data: messageToSend } = await openpgp.encrypt({
+          message: openpgp.message.fromText(
+            JSON.stringify({
+              itsMine: false,
+              contact: socketQuery.userName,
+              message
+            })
+          ),
+          publicKeys: (await openpgp.key.readArmored(publicKeyTo)).keys,
+          privateKeys: [globalAny.__PRIVATE_KEY_SERVER__]
+        });
+
+        Redis.hmset(`Message:to:${to}:from:${socketQuery.userName}:${id}`, {
+          id,
+          message: messageToSend,
+          dateSended: new Date().getTime()
+        });
+
+        socket.to(to).emit('message:receive', { id, message: messageToSend });
+        console.timeEnd('New Message');
+        return true;
+      }
+    );
+
+    socket.on(
+      'message:new:received',
+      async (messageReceived, timestamp = 1000): Promise<void | boolean> => {
+        const publicKeyUser = await new Promise(resolve => {
+          Redis.hmget(
+            `Users:${socketQuery.userName}:data`,
+            'publicKey',
+            (err, data) => resolve(data[0])
+          );
+        });
+
+        const decrypted = await openpgp.decrypt({
+          message: await openpgp.message.readArmored(messageReceived),
+          publicKeys: (await openpgp.key.readArmored(publicKeyUser)).keys,
+          privateKeys: [globalAny.__PRIVATE_KEY_SERVER__]
+        });
+
+        if (!decrypted.signatures[0].valid) {
+          return false;
+        }
+
+        const message: { id: string; to: string; from: string } = JSON.parse(
+          decrypted.data
+        );
+
+        Redis.del(
+          `Message:to:${message.to}:from:${message.from}:${message.id}`
+        );
+
+        return true;
+      }
+    );
+  });
 };
 
 export default conversation;
