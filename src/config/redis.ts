41a4704b53767c13c56cae2748976d15d49a116e
Marcon Willian Oliveira Neves <oi@marconwillian.dev>
2021-01-30 21:42:58 -0300

    chore: Update Account Routes

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,5 +1,5 @@
 export default {
-    url: process.env.REDIS_URL
-        ? process.env.REDIS_URL
-        : `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+  url: process.env.REDIS_URL
+    ? process.env.REDIS_URL
+    : `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
 };
--- src/routes/account.routes.ts
+++ src/routes/account.routes.ts
@@ -10,65 +10,70 @@ const Account = Router();
 Account.post('/', async (request, response) => {
   const { user, key: keyEncrypted } = request.body;
 
-  const publicKeyClient = await fs.readFileSync('keys/client.cpu', 'utf-8');
-  const privateKeyServer = await fs.readFileSync('keys/server.cpr', 'utf-8');
+  try {
+    const publicKeyClient = await fs.readFileSync('keys/client.cpu', 'utf-8');
+    const privateKeyServer = await fs.readFileSync('keys/server.cpr', 'utf-8');
 
-  const {
-    keys: [privateKey]
-  } = await openpgp.key.readArmored(privateKeyServer);
-  await privateKey.decrypt(process.env.PGP_SERVER_TOKEN || '');
+    const {
+      keys: [privateKey]
+    } = await openpgp.key.readArmored(privateKeyServer);
+    await privateKey.decrypt(process.env.PGP_SERVER_TOKEN || '');
 
-  const key = await openpgp.decrypt({
-    message: await openpgp.message.readArmored(keyEncrypted),
-    publicKeys: (await openpgp.key.readArmored(publicKeyClient)).keys,
-    privateKeys: [privateKey]
-  });
-
-  if (!key.signatures[0].valid) {
-    return response.status(400).json({
-      status: 'error',
-      messageCode: '2',
-      message: 'Assinatura PGP inválida!'
+    const key = await openpgp.decrypt({
+      message: await openpgp.message.readArmored(keyEncrypted),
+      publicKeys: (await openpgp.key.readArmored(publicKeyClient)).keys,
+      privateKeys: [privateKey]
     });
-  }
 
-  const publicKey = (await openpgp.key.readArmored(key.data)).keys[0];
+    if (!key.signatures[0].valid) {
+      return response.status(400).json({
+        status: 'error',
+        messageCode: '2',
+        message: 'Assinatura PGP inválida!'
+      });
+    }
 
-  console.log(publicKey.getUserIds());
+    const publicKey = (await openpgp.key.readArmored(key.data)).keys[0];
 
-  if (
-    publicKey.getUserIds()[0] !== `${user} <${user}.user@chatsecret.mh4sh.dev>`
-  ) {
-    return response.status(400).json({
-      status: 'error',
-      messageCode: '2',
-      message: 'Assinatura PGP inválida!'
+    console.log(publicKey.getUserIds());
+
+    if (
+      publicKey.getUserIds()[0] !==
+      `${user} <${user}.user@chatsecret.mh4sh.dev>`
+    ) {
+      return response.status(400).json({
+        status: 'error',
+        messageCode: '2',
+        message: 'Assinatura PGP inválida!'
+      });
+    }
+
+    const hasUser = await new Promise(resolve => {
+      Redis.hmget(`Users:${user}:data`, 'status', (err, data) =>
+        resolve(data[0])
+      );
     });
-  }
 
-  const hasUser = await new Promise(resolve => {
-    Redis.hmget(`Users:${user}:data`, 'status', (err, data) =>
-      resolve(data[0])
-    );
-  });
+    if (hasUser !== null) {
+      return response.status(400).json({
+        status: 'error',
+        messageCode: '1',
+        message: 'Usuário já esta em uso!'
+      });
+    }
 
-  if (hasUser !== null) {
-    return response.status(400).json({
-      status: 'error',
-      messageCode: '1',
-      message: 'Usuário já esta em uso!'
+    Redis.hmset(`Users:${user}:data`, {
+      user,
+      emailId: `${user} <${user}.user@chatsecret.mh4sh.dev>`,
+      status: 'active',
+      publicKey: key.data,
+      lastAccess: new Date().getTime()
     });
+
+    return response.status(201).json({});
+  } catch (err) {
+    return response.status(400).json({ error: JSON.stringify(err) });
   }
-
-  Redis.hmset(`Users:${user}:data`, {
-    user,
-    emailId: `${user} <${user}.user@chatsecret.mh4sh.dev>`,
-    status: 'active',
-    publicKey: key.data,
-    lastAccess: new Date().getTime()
-  });
-
-  return response.status(201).json({});
 });
 
 export default Account;
