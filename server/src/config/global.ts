66ef32b3a03ac7dad41418e6e4f9ae68ddce66cb
Marcon Willian Oliveira Neves <oi@marconwillian.dev>
2021-01-31 18:06:57 -0300

    Mv server to folder

---
+++ server/src/config/global.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-underscore-dangle */
+import fs from 'fs';
+import path from 'path';
+import * as openpgp from 'openpgp';
+
+const globalAny: any = global;
+
+const globalVars = async (): Promise<void> => {
+  const publicKeyClientArmored = await fs.readFileSync(
+    path.resolve(__dirname, '..', '..', 'keys', 'client.cpu'),
+    'utf-8'
+  );
+  const [publicKeyClient] = (
+    await openpgp.key.readArmored(publicKeyClientArmored)
+  ).keys;
+
+  const privateKeyServerArmored = await fs.readFileSync(
+    path.resolve(__dirname, '..', '..', 'keys', 'server.cpr'),
+    'utf-8'
+  );
+  const {
+    keys: [privateKeyServer]
+  } = await openpgp.key.readArmored(privateKeyServerArmored);
+  await privateKeyServer.decrypt(process.env.PGP_SERVER_TOKEN || '');
+
+  globalAny.__PUBLIC_KEY_CLIENT__ = publicKeyClient;
+  globalAny.__PRIVATE_KEY_SERVER__ = privateKeyServer;
+};
+
+export default globalVars;
---
+++ server/src/index.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+
+import server from './server';
+
+server();
+// senha-chave
+
+// https://www.youtube.com/watch?v=mDc5xueL7rs&list=PLmIA3VZysEqQCi5xOJJDb5QIYgHwPhf_D&index=43
+// Cursos sobre IndexedDB
